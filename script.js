c40e4d36e861eea7b1a8fb819d7cc180f3e157fa
ChristophertheDeveloper <38364484+ChristophertheDeveloper@users.noreply.github.com>
2023-03-07 00:53:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let meterbtn = document.getElementById('meter');
+let feetbtn = document.getElementById('feet');
+
+meterbtn.addEventListener('click', function() {
+
+	let input = document.getElementById('input').value;
+	document.getElementById('output').value = input / 3.281 + " Meters";
+})
+
+feetbtn.addEventListener('click', function() {
+
+	let input = document.getElementById('input').value;
+
+	document.getElementById('output').value = input * 3.281 + " Feet";
+
+
+
+})
